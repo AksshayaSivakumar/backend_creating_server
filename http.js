4c3fc8694890984cd3deeba762869a93903c7b59
AksshayaSivakumar <aksshayapugal@gmail.com>
2023-11-07 14:07:06 +0530

    request and response

--- http.js
+++ http.js
@@ -1,7 +1,20 @@
-const http=require('http')
-http.createServer(function(req,res)
-{
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.write('Aksshaya');
-    res.end();
+const http=require('http');
+const url=require('url');
+const fs=require('fs')
+
+http.createServer(function(req,res){
+    var q=url.parse(req.url,true);
+    var filename='.'+q.pathname;
+    fs.readFile(filename,function(err,data){
+        if(err)
+        {
+            res.writeHead(404,{'content-type':'text/html'});
+            return res.end('404 not found')
+        }
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
 }).listen(4000);
+
+
