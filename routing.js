fd7ad39b5d7e09714f87cefd653efae770734986
AksshayaSivakumar <aksshayapugal@gmail.com>
2023-11-22 15:22:54 +0530

    intro to middlewares

--- routing.js
+++ routing.js
@@ -1,14 +1,22 @@
 
 const express= require('express');
+const bodyparser=require('body-parser');
 
 const app=express();
-app.use((req,res,next)=>{
-    console.log("middleware1");
-    next();
+app.use(bodyparser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    
+   res.send('<form action="/product" method="post"><input type="text" name="title" placeholder="add product"><input type="text" name="title" placeholder="add size"><button type="submit">submit</button><br><br></form>')
+   
 });
 
-app.use((req,res,next)=>{
-    console.log("middleware3");
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    
     res.send('<h1>hello to nodejs</h1>')
 });
 
